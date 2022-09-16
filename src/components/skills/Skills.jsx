2110e2bb90e5fa3c4f1fdb4126e4e3c251ef869e
Brian <oshinbolubrian@gmail.com>
2022-09-16 01:18:25 +0100

    My skills Section Completed

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -14,28 +14,49 @@ export default function Skills(){
 			<img src="https://img.icons8.com/ios-filled/70/000000/html-5--v1.png"/>
 			<div className='progress-info'>
 				<p>HTML</p>
-				<progress id="file" value="89" max="100"> 89% </progress>
+				<progress id="file" value="95" max="100"> 85% </progress>
 			</div>
 		</div>
 		<div className='skill-progress'>
 			<img src="https://img.icons8.com/ios-filled/70/000000/html-5--v1.png"/>
 			<div className='progress-info'>
-				<p>HTML</p>
+				<p>CSS</p>
+				<progress id="file" value="90" max="100"> 90% </progress>
+			</div>
+		</div>
+		<div className='skill-progress'>
+			<img src="https://img.icons8.com/ios-filled/70/000000/javascript-logo.png"/>
+			<div className='progress-info'>
+				<p>JAVSCRIPT</p>
 				<progress id="file" value="89" max="100"> 89% </progress>
 			</div>
 		</div>
 		<div className='skill-progress'>
-			<img src="https://img.icons8.com/ios-filled/70/000000/html-5--v1.png"/>
+			<img src="https://img.icons8.com/dotty/70/000000/react.png"/>
 			<div className='progress-info'>
-				<p>HTML</p>
-				<progress id="file" value="89" max="100"> 89% </progress>
+				<p>REACT</p>
+				<progress id="file" value="70" max="100"> 70% </progress>
 			</div>
 		</div>
 		<div className='skill-progress'>
-			<img src="https://img.icons8.com/ios-filled/70/000000/html-5--v1.png"/>
+			<img src="https://img.icons8.com/ios-filled/70/000000/python.png"/>
 			<div className='progress-info'>
-				<p>HTML</p>
-				<progress id="file" value="89" max="100"> 89% </progress>
+				<p>PYTHON</p>
+				<progress id="file" value="90" max="100"> 90% </progress>
+			</div>
+		</div>
+		<div className='skill-progress'>
+			<img src="https://img.icons8.com/ios-filled/70/000000/git.png"/>
+			<div className='progress-info'>
+				<p>GIT</p>
+				<progress id="file" value="70" max="100"> 70% </progress>
+			</div>
+		</div>
+		<div className='skill-progress'>
+			<img src="https://img.icons8.com/small/70/000000/bash.png"/>
+			<div className='progress-info'>
+				<p>BASH</p>
+				<progress id="file" value="70" max="100"> 70% </progress>
 			</div>
 		</div>
 
